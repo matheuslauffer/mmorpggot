d1e5510f901b08b217e4f45d40c1c8f567c43274
Matheus Lauffer <matheuslauffer1@gmail.com>
2017-10-23 23:52:23 -0200

    mmorpg

---
+++ config/dbConnection.js
@@ -0,0 +1,15 @@
+// importar o mongodb
+var mongo = require('mongodb');
+
+var connMongoDB = function(){
+  var db = new mongo.Db(
+    'got',
+    new mongo.Server('localhost',27017, {}),
+    {}
+  );
+  return db;
+}
+
+module.exports = function(){
+  return connMongoDB;
+}
